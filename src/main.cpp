fb8da8c92e709647445cbd8990b894273eedddef
Joshua Bystrom <jbystr2005@gmail.com>
2026-09-24 16:33:58 -0500

    initialized computational nx x ny grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
+#include <AMReX.H>
 #include "GISProcessor.H"
 #include "Simulation.H"
 
-int main()
+int main(int argc, char* argv[])
 {
-    GISProcessor processor;
+    amrex::Initialize(argc, argv);
+    {
+        GISProcessor processor;
 
-    GISData data = processor.load("data/export.geojson");
+        GISData data = processor.load("data/export.geojson");
 
-    Simulation simulation(data);
+        Simulation simulation(data);
 
-    simulation.initialize();
-
-    cout << "Width: " << data.width << "\n";
-    cout << "Height: " << data.height << "\n";
-    cout << "Number of buildings: " << data.buildings.size() << "\n";
-
+        simulation.initialize();
+    }
+    amrex::Finalize();
     return 0;
 }
